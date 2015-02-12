5648e05fc252840d9dd1eed27c1044d334f5d4a1
Ahlisen <ahlisen93@gmail.com>
2015-02-12 19:28:52 +0100

    Minor changes to layout

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -17,10 +17,10 @@ var ExampleView = function (container, model) {
 	var dishes = model.getAllDishes(this.dishType.val());
 
 	for(i = 0; i < dishes.length; i++ ) {
-		this.dishList.append("<div class='col-md-3' id='dishItem'>"+
+		this.dishList.append("<div class='col-md-3'><div class='dishItem'>"+
 			"<div class='row'><img src='images/"+ dishes[i].image +"'/></div>"+
 			"<div class='row'><h2>"+ dishes[i].name +"</h2></div>"+
-			"<div class='row'><h3>"+ dishes[i].description +"</h3></div></div>");
+			"<div class='row'><h3>"+ dishes[i].description +"</h3></div></div></div>");
 	}
 	
 	
