8c842a2fca32917d93ea456248535006f8461755
Ahlisen <ahlisen93@gmail.com>
2015-02-12 19:25:08 +0100

    Dish list with items

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -3,12 +3,26 @@ var ExampleView = function (container, model) {
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	this.numberOfGuests = container.find("#numberOfGuests");
+	/*this.numberOfGuests = container.find("#numberOfGuests");
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
 	
 	this.numberOfGuests.val(model.getNumberOfGuests());
 	this.numberOfGuests.html(this.numberOfGuests.val());
+	*/
+	this.dishType = container.find("#dishType");
+
+	this.dishList = container.find("#dishList");
+
+	var dishes = model.getAllDishes(this.dishType.val());
+
+	for(i = 0; i < dishes.length; i++ ) {
+		this.dishList.append("<div class='col-md-3' id='dishItem'>"+
+			"<div class='row'><img src='images/"+ dishes[i].image +"'/></div>"+
+			"<div class='row'><h2>"+ dishes[i].name +"</h2></div>"+
+			"<div class='row'><h3>"+ dishes[i].description +"</h3></div></div>");
+	}
+	
 	
 }
  
