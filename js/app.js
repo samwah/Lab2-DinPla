be1c783c48ce5fc9666594dc9d87360d9c4739e6
Ahlisen <ahlisen93@gmail.com>
2015-02-12 21:09:01 +0100

    More pages and views

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(function() {
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
+	var previewView = new PreviewView($("#previewView"), model);
 	var myDinnerView = new MyDinnerView($("#myDinnerView"), model);
 	var oneDishView = new OneDishView($("#oneDishView"), model);
 
