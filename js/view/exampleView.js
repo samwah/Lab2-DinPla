89303aef79ef3f6f1324522a2807e22448f2b011
samwah <samuel.wahlstedt@gmail.com>
2015-02-17 16:43:03 +0100

    Starting grounds

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -1,28 +1,12 @@
 //ExampleView Object constructor
-var ExampleView = function (container, model) {
+var ExampleView = function (container) {
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	/*this.numberOfGuests = container.find("#numberOfGuests");
+	this.numberOfGuests = container.find("#numberOfGuests");
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
 	
-	this.numberOfGuests.val(model.getNumberOfGuests());
-	this.numberOfGuests.html(this.numberOfGuests.val());
-	*/
-	this.dishType = container.find("#dishType");
-
-	this.dishList = container.find("#dishList");
-
-	var dishes = model.getAllDishes(this.dishType.val());
-
-	for(i = 0; i < dishes.length; i++) {
-		this.dishList.append("<div class='col-md-3'><div class='dishItem'>"+
-			"<div class='row'><a href='lasagne.html'><img src='images/"+ dishes[i].image +"'/></a></div>"+
-			"<div class='row'><h2>"+ dishes[i].name +"</h2></div>"+
-			"<div class='row'><h3>"+ dishes[i].description +"</h3></div></div></div>");
-	}
-	
+	this.numberOfGuests.html("Hello World");
 	
 }
- 
