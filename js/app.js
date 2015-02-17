9999f2e680c2ef8f899958138bed00051462e303
samwah <samuel.wahlstedt@gmail.com>
2015-02-17 20:27:06 +0100

    2

--- js/app.js
+++ js/app.js
@@ -4,12 +4,19 @@ $(function() {
 	
 	//And create the needed controllers and views
 
-	//testing...
+	//test
 	var exampleView = new ExampleView($("#exampleView"), model);
 	var exampleViewController = new ExampleViewController(exampleView,model);
 
-	var myDinnerView 
 
+	//not test
+
+	var welcomeView = new welcomeView($("#welcomeView"), model);
+	var welcomeViewController = new welcomeViewController(welcomeView,model);
+
+
+
+//old stuff
 	var previewView = new PreviewView($("#previewView"), model);
 	var preperationView = new PreperationView($("#preperationView"), model);
 	var myDinnerView = new MyDinnerView($("#myDinnerView"), model);
