dc9397346cc855b4dcb99106eb71e48f76883c8b
samwah <samuel.wahlstedt@gmail.com>
2015-02-24 16:19:56 +0100

    People +- button works now

--- js/view/myDinnerView.js
+++ js/view/myDinnerView.js
@@ -3,27 +3,26 @@ var MyDinnerView = function (container, model) {
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
+
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
 	this.Debug = container.find("#Debug");
-
 	
-	this.numberOfGuests.val(model.getNumberOfGuests());
-	this.numberOfGuests.html(this.numberOfGuests.val());
+	//this.numberOfGuests.val(model.getNumberOfGuests());
+	//this.numberOfGuests.html(this.numberOfGuests.val());
 
 	model.addObserver(this);
 
-	var updateGuests = function() {
+	this.updateGuests = function() {
 		this.numberOfGuests.val(model.getNumberOfGuests());
-		this.numberOfGuests.html = this.numberOfGuests;
+		this.numberOfGuests.html(this.numberOfGuests.val());
 	};
-
 
 	this.update = function(obj) {
-		updateGuests();
+		this.updateGuests();
 	};
 
-	updateGuests();
+	this.updateGuests();
 }
  
