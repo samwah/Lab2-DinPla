37b1452200c9387da0f8f78ddebee549294e1cdc
Ahlisen <ahlisen93@gmail.com>
2015-03-23 18:51:03 +0100

    more

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,11 +1,11 @@
 // Search controller that we use whenever we have a search inputs
 // and search results
 dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
-
+	
 	$scope.search = function(query) {
 		$scope.status = "Searching...";
 		Dinner.DishSearch.get({title_kw:query},function(data){
-			$scope.dishes = data.Results;
+			$scope.dishes=data.Results;
 			$scope.status = "Showing " + data.Results.length + " results";
 		},function(data){
 			$scope.status = "There was an error";
